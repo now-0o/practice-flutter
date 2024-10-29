0368b748ae77276171090a875a7ab9c3b040f222
now-0o <dlwpdn0704@gmail.com>
2024-10-29 23:14:08 +0900

    feat: 데일리 플래너 기본 화면 완성

--- 20241029/daily_planner/lib/main.dart
+++ 20241029/daily_planner/lib/main.dart
@@ -22,7 +22,8 @@ class _AppState extends State<App> {
 
   void _calculateDays() {
     DateTime now = DateTime.now();
-    int nextDay = DateTime(now.year, now.month, now.day + 1).day;
+    //int nextDay = DateTime(now.year, now.month, now.day + 1).day;
+    int nextDay = 17;
     int lastDay = DateTime(now.year, now.month + 1, 0).day;
 
     days = List.generate(lastDay - nextDay + 1, (index) => nextDay + index);
@@ -57,14 +58,14 @@ class _AppState extends State<App> {
               ),
               const SizedBox(height: 30),
               const Text(
-                '요일 날짜',
+                'MONDAY 16',
                 style: TextStyle(
                   color: Color(0xfff0f0f0),
                   fontSize: 20,
                   fontWeight: FontWeight.w500,
                 ),
               ),
-              const SizedBox(height: 10),
+              const SizedBox(height: 5),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
@@ -122,6 +123,373 @@ class _AppState extends State<App> {
                   ),
                 ],
               ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                  color: Color(0xfffef754),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.only(top: 30, bottom: 20),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '11',
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.w500,
+                                      letterSpacing: -2,
+                                      height: 0.8,
+                                    ),
+                                  ),
+                                  Text(
+                                    '30',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Text(
+                                    '|',
+                                    style: TextStyle(
+                                      fontSize: 36,
+                                      fontWeight: FontWeight.w100,
+                                      height: 0.8,
+                                    ),
+                                  ),
+                                  Text(
+                                    '12',
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Text(
+                                    '20',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                      height: 0.8,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'DESIGN',
+                                  style: TextStyle(
+                                    fontSize: 64,
+                                    fontWeight: FontWeight.w500,
+                                    height: 0.6,
+                                  ),
+                                ),
+                                Text(
+                                  'MEETING',
+                                  style: TextStyle(
+                                    fontSize: 64,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 80,
+                          ),
+                          Text(
+                            'ALEX',
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            'HELENA',
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            'NANA',
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                  color: Color(0xff9c6bce),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.only(top: 30, bottom: 20),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '12',
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.w500,
+                                      height: 0.8,
+                                    ),
+                                  ),
+                                  Text(
+                                    '35',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Text(
+                                    '|',
+                                    style: TextStyle(
+                                      fontSize: 36,
+                                      fontWeight: FontWeight.w100,
+                                      height: 0.8,
+                                    ),
+                                  ),
+                                  Text(
+                                    '14',
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Text(
+                                    '10',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                      height: 0.8,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'DAILY',
+                                  style: TextStyle(
+                                    fontSize: 64,
+                                    fontWeight: FontWeight.w500,
+                                    height: 0.6,
+                                  ),
+                                ),
+                                Text(
+                                  'PROJECT',
+                                  style: TextStyle(
+                                    fontSize: 64,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 80,
+                          ),
+                          Text(
+                            'ME',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            'RICHARD',
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            'CIRY',
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                  color: Color(0xffbcee4b),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.only(top: 30, bottom: 20),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '15',
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.w500,
+                                      height: 0.8,
+                                    ),
+                                  ),
+                                  Text(
+                                    '00',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Text(
+                                    '|',
+                                    style: TextStyle(
+                                      fontSize: 36,
+                                      fontWeight: FontWeight.w100,
+                                      height: 0.8,
+                                    ),
+                                  ),
+                                  Text(
+                                    '16',
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Text(
+                                    '30',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                      height: 0.8,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'WEEKLY',
+                                  style: TextStyle(
+                                    fontSize: 64,
+                                    fontWeight: FontWeight.w500,
+                                    height: 0.6,
+                                  ),
+                                ),
+                                Text(
+                                  'PLANNER',
+                                  style: TextStyle(
+                                    fontSize: 64,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 80,
+                          ),
+                          Text(
+                            'DEN',
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            'NANA',
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            'MARK',
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
